ddd55c41692c0c8c4ca6932a63ea4dcc37526054
Dale Alleshouse <dalealleshouse@gmail.com>
2023-11-07 19:06:13 -0500

    WIP

--- Algorithms.Tests/DataStructures/Heap/Delete.cs
+++ Algorithms.Tests/DataStructures/Heap/Delete.cs
@@ -1,72 +1,71 @@
-namespace Algorithms.Tests.DataStructures.Heap
+namespace Algorithms.Tests.DataStructures.Heap;
+
+using System;
+using Algorithms.DataStructures;
+using Xunit;
+
+public class Delete
 {
-    using System;
-    using Algorithms.DataStructures;
-    using Xunit;
-
-    public class Delete
+    [Fact]
+    public void RejectNull()
     {
-        [Fact]
-        public void RejectNull()
+        var sut = new Heap<object>((x, y) => -1, 10);
+        Assert.Throws<ArgumentNullException>(() => sut.Delete(null));
+    }
+
+    [Fact]
+    public void ThorwIfItemDoesNotExist()
+    {
+        var sut = SutFactory.MaxHeap(10, new int[] { 5, 10 });
+
+        Assert.Throws<ArgumentException>(() => sut.Delete(15));
+    }
+
+    [Fact]
+    public void Reprioritizes()
+    {
+        var data = new int[] { 25, 5, 15, 10, 20 };
+        var sut = SutFactory.MaxHeap(10, data);
+
+        sut.Delete(25);
+        Assert.Equal(20, sut.Peek());
+    }
+
+    [Fact]
+    public void DecrementsItemCount()
+    {
+        var data = new int[] { 25, 5, 15, 10, 20 };
+        var n = data.Length;
+        var sut = SutFactory.MaxHeap(10, data);
+
+        Assert.Equal((uint)n, sut.ItemCount);
+
+        for (var i = 0; i < n; i++)
         {
-            var sut = new Heap<object>((x, y) => -1, 10);
-            Assert.Throws<ArgumentNullException>(() => sut.Delete(null));
+            Assert.Equal((uint)(n - i), sut.ItemCount);
+            sut.Delete(data[i]);
         }
 
-        [Fact]
-        public void ThorwIfItemDoesNotExist()
-        {
-            var sut = SutFactory.MaxHeap(10, new int[] { 5, 10 });
+        Assert.Equal(0U, sut.ItemCount);
+    }
 
-            Assert.Throws<ArgumentException>(() => sut.Delete(15));
-        }
+    [Fact]
+    public void DeletesOnlyOneOfDuplicate()
+    {
+        var data = new int[] { 25, 5, 5, 10 };
+        var n = data.Length;
+        var sut = SutFactory.MaxHeap(10, data);
 
-        [Fact]
-        public void Reprioritizes()
-        {
-            var data = new int[] { 25, 5, 15, 10, 20 };
-            var sut = SutFactory.MaxHeap(10, data);
+        sut.Delete(5);
+        Assert.Equal(3U, sut.ItemCount);
 
-            sut.Delete(25);
-            Assert.Equal(20, sut.Peek());
-        }
+        Assert.Equal(25, sut.Extract());
+        Assert.Equal(2U, sut.ItemCount);
 
-        [Fact]
-        public void DecrementsItemCount()
-        {
-            var data = new int[] { 25, 5, 15, 10, 20 };
-            var n = data.Length;
-            var sut = SutFactory.MaxHeap(10, data);
+        Assert.Equal(10, sut.Extract());
+        Assert.Equal(1U, sut.ItemCount);
 
-            Assert.Equal((uint)n, sut.ItemCount);
-
-            for (var i = 0; i < n; i++)
-            {
-                Assert.Equal((uint)(n - i), sut.ItemCount);
-                sut.Delete(data[i]);
-            }
-
-            Assert.Equal(0U, sut.ItemCount);
-        }
-
-        [Fact]
-        public void DeletesOnlyOneOfDuplicate()
-        {
-            var data = new int[] { 25, 5, 5, 10 };
-            var n = data.Length;
-            var sut = SutFactory.MaxHeap(10, data);
-
-            sut.Delete(5);
-            Assert.Equal(3U, sut.ItemCount);
-
-            Assert.Equal(25, sut.Extract());
-            Assert.Equal(2U, sut.ItemCount);
-
-            Assert.Equal(10, sut.Extract());
-            Assert.Equal(1U, sut.ItemCount);
-
-            Assert.Equal(5, sut.Extract());
-            Assert.Equal(0U, sut.ItemCount);
-        }
+        Assert.Equal(5, sut.Extract());
+        Assert.Equal(0U, sut.ItemCount);
     }
 }
--- Algorithms.Tests/DataStructures/Heap/Exists.cs
+++ Algorithms.Tests/DataStructures/Heap/Exists.cs
@@ -1,58 +1,57 @@
-namespace Algorithms.Tests.DataStructures.Heap
+namespace Algorithms.Tests.DataStructures.Heap;
+
+using System;
+using Algorithms.DataStructures;
+using Xunit;
+
+public class Exists
 {
-    using System;
-    using Algorithms.DataStructures;
-    using Xunit;
-
-    public class Exists
+    [Fact]
+    public void RejectNull()
     {
-        [Fact]
-        public void RejectNull()
-        {
-            var sut = new Heap<object>((x, y) => -1, 10);
-            Assert.Throws<ArgumentNullException>(() => sut.Exists(null));
-        }
+        var sut = new Heap<object>((x, y) => -1, 10);
+        Assert.Throws<ArgumentNullException>(() => sut.Exists(null));
+    }
 
-        [Fact]
-        public void ReturnsTrueWhenItemExist()
-        {
-            var sut = SutFactory.MaxHeap(10, new int[] { 5, 10 });
+    [Fact]
+    public void ReturnsTrueWhenItemExist()
+    {
+        var sut = SutFactory.MaxHeap(10, new int[] { 5, 10 });
 
-            Assert.True(sut.Exists(5));
-            Assert.True(sut.Exists(10));
-        }
+        Assert.True(sut.Exists(5));
+        Assert.True(sut.Exists(10));
+    }
 
-        [Fact]
-        public void ReturnsFalseWhenItemDoesNotExist()
-        {
-            var sut = SutFactory.MaxHeap(10, new int[] { 5, 10 });
+    [Fact]
+    public void ReturnsFalseWhenItemDoesNotExist()
+    {
+        var sut = SutFactory.MaxHeap(10, new int[] { 5, 10 });
 
-            Assert.False(sut.Exists(15));
-            Assert.False(sut.Exists(100));
-        }
+        Assert.False(sut.Exists(15));
+        Assert.False(sut.Exists(100));
+    }
 
-        [Fact]
-        public void ReturnsFalseAfterItemRemoved()
-        {
-            var sut = SutFactory.MaxHeap(10, new int[] { 5, 10 });
+    [Fact]
+    public void ReturnsFalseAfterItemRemoved()
+    {
+        var sut = SutFactory.MaxHeap(10, new int[] { 5, 10 });
 
-            Assert.True(sut.Exists(10));
-            Assert.Equal(10, sut.Extract());
-            Assert.False(sut.Exists(10));
-        }
+        Assert.True(sut.Exists(10));
+        Assert.Equal(10, sut.Extract());
+        Assert.False(sut.Exists(10));
+    }
 
-        [Fact]
-        public void ReturnsTrueAfterDuplicateItemRemoved()
-        {
-            var sut = SutFactory.MaxHeap(10, new int[] { 5, 10, 10 });
+    [Fact]
+    public void ReturnsTrueAfterDuplicateItemRemoved()
+    {
+        var sut = SutFactory.MaxHeap(10, new int[] { 5, 10, 10 });
 
-            Assert.True(sut.Exists(10));
+        Assert.True(sut.Exists(10));
 
-            Assert.Equal(10, sut.Extract());
-            Assert.True(sut.Exists(10));
+        Assert.Equal(10, sut.Extract());
+        Assert.True(sut.Exists(10));
 
-            Assert.Equal(10, sut.Extract());
-            Assert.False(sut.Exists(10));
-        }
+        Assert.Equal(10, sut.Extract());
+        Assert.False(sut.Exists(10));
     }
 }
